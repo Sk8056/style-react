dcc719f40936e60aa6a2a189c7f0de2e3a4f59ee
Sk8056 <sarangkumar8056@gmail.com>
2021-09-16 18:59:30 +0530

    completed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,132 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
 import { FC } from "react";
 import styled from "styled-components";
 
-{
-  /* There are many variation of passages of Lorem Ipsum available, but the
-          majority have suffered alteration in some form, by injected humour, or
-      randomised words which don't look even slightly believable*/
-}
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+const Title = styled.h1`
+  margin-bottom: 30px;
+`;
 
-const Container = styled.div``;
+const List = styled.ul`
+  list-style: none;
+  padding: 0px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+const Logo = styled.h1``;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div<{ color: string }>`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 10px;
+`;
 
 const Footer: FC<{}> = () => {
-  return <Container>Footer redeploy test</Container>;
+  return (
+    <Container>
+      <Left>
+        <Logo>React.</Logo>
+        <Description>
+          There are many variation of passages of Lorem Ipsum available, but the
+          majority have suffered alteration in some form, by injected humour, or
+          randomised words which don't look even slightly believable*/
+        </Description>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405f">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="385999">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="385999">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>WishList</ListItem>
+          <ListItem>WishList</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} />
+          622 Dixie Path, South Tobinchester 98335
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} />
+          +1 234 56 78
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} />
+          contact@react.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
 };
 
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Payment = styled.img`
+  width: 50%;
+`;
 export default Footer;
